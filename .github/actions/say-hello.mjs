3f2d0c0f332fc599899553b9225bc4d7005587fe
Lucas Nørgård <lucasnrgaard@gmail.com>
2024-04-20 09:07:28 +0200

    initial commit

---
+++ .github/actions/say-hello.mjs
@@ -0,0 +1,7 @@
+// @ts-check
+
+/** @param {import('github-script').AsyncFunctionArguments} AsyncFunctionArguments */
+export default async ({ core }) => {
+  core.debug('Running something at the moment')
+  core.info('Hello, World!')
+}
