683ecec0537ae5f0b058679c331d54499d52f4e9
Lucas Nørgård <lucasnrgaard@gmail.com>
2024-05-10 08:04:23 +0200

    feat: check for published versions on npm

--- src/crons/mentions/commands/release.ts
+++ src/crons/mentions/commands/release.ts
@@ -18,8 +18,36 @@ export default {
     }
 
     // parse the content of the package.json
-    // eslint-disable-next-line node/prefer-global/buffer
-    const packageJsonContent = Buffer.from(packageJson.content, 'base64').toString('utf-8')
+    const packageJsonContent = JSON.parse(atob(packageJson.content))
+
+    const packageName = packageJsonContent.name
+    const currentVersion = packageJsonContent.version
+    const nextVersion = inc(currentVersion, 'patch')
+
+    if (!packageName || !currentVersion || !nextVersion) {
+      throw new Error('failed to parse package.json')
+    }
+
+    // list all npm version
+    const npm = await fetch(`https://registry.npmjs.org/${packageName}`, {
+      headers: {
+        'User-Agent': 'robobub actions',
+      },
+    }).then((res) => res.json())
+
+    if (!npm || typeof npm !== 'object' || !('versions' in npm) || typeof npm.versions !== 'object' || !npm.versions) {
+      throw new Error('failed to fetch npm versions')
+    }
+
+    const hasNextVersion = nextVersion in npm.versions
+
+    let comment = 'Hi, @luxass. \n\n'
+
+    if (hasNextVersion) {
+      comment += `The version ${nextVersion} is already published.\n\nThese are the versions that are already published:\n\n - ${Object.keys(npm.versions).join('\n - ')}`
+    } else {
+      comment += `I'm going to try and create a release for this version soon. \n\nBumping package version from ${currentVersion} -> ${nextVersion}.`
+    }
 
     // if no command is found, will just say hallo to the user
     const { data: createdComment } = await octokit.request(
@@ -28,7 +56,7 @@ export default {
         owner: mention.repository.owner.login,
         repo: mention.repository.name,
         issue_number: issueNumber,
-        body: `I will release it soon: current version ${JSON.parse(packageJsonContent).version} -> ${inc(JSON.parse(packageJsonContent).version, 'patch')}`,
+        body: comment,
         headers: {
           'X-GitHub-Api-Version': '2022-11-28',
         },
